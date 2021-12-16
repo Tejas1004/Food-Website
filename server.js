67154b9a09b01cc3f0266d07da0c0e1449f1d3a3
Your Name <tejaszanzane2001@gmail.com>
2021-12-16 19:07:00 +0530

    add auth page done

--- server.js
+++ server.js
@@ -8,10 +8,19 @@ const PORT= process.env.PORT||5000
 //Assets
 app.use(express.static('public'))
 
-app.get('/',(req,res)=>{
-    res.render('home.ejs')
-})
 
+app.get('/',(req,res)=>{
+    res.render("home.ejs")
+})
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart.ejs')
+})
+app.get('/login',(req,res)=>{
+    res.render('auth/login.ejs')
+})
+app.get('/register',(req,res)=>{
+    res.render('auth/register.ejs')
+})
 //Template Engine Setup
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
