88e119d8abc45db6c070a9d5057f2040a4ceaf52
Your Name <tejaszanzane2001@gmail.com>
2021-12-20 18:47:40 +0530

    Add To Cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,25 @@
 import axios from 'axios'
+import Noty from 'noty'
 let addToCart=document.querySelectorAll('.add-to-cart')
+let cartCounter=document.querySelector(".cartCounter")
 
 function updateCart(pizza){
     axios.post('/update-cart',pizza)
     .then(res=>{
         console.log(res)
+        cartCounter.innerText=res.data.totalQty
+
+        new Noty({
+            type:'success',
+            timeout:1000,
+            text:"item added in your cart 👍"
+        }).show();
+    }).catch(err=>{
+        new Noty({
+            type:error,
+            timeout:1000,
+            text:"something went wrong!try again later"
+        })
     })
 }
 addToCart.forEach((btn) => {
--- server.js
+++ server.js
@@ -1,69 +1,76 @@
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const ejs = require('ejs')
-const expressLayout = require('express-ejs-layouts')
-const path=require('path')
-const mongoose=require('mongoose')
-const session=require('express-session')
-const flash=require('express-flash')
-const MongoDbStore = require('connect-mongo')
-const PORT= process.env.PORT||5000
-
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+const path = require("path");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("express-flash");
+const MongoDbStore = require("connect-mongo");
+const PORT = process.env.PORT || 5000;
 
 //Database Connected
-const dburl="mongodb+srv://Foodweb:Foodweb@website.1ktid.mongodb.net/pizza"
+const dburl = "mongodb+srv://Foodweb:Foodweb@website.1ktid.mongodb.net/pizza";
 mongoose.connect(dburl);
 const connection = mongoose.connection;
 // When successfully connected
- connection.on('connected', function () {  
-    console.log('Database Connected ' );
-  }); 
-  
-  // If the connection throws an error
-  connection.on('error',function (err) {  
-    console.log('Database is not connected because of '+err);
-  }); 
-  
-  // When the connection is disconnected
-  connection.on('disconnected', function () {  
-    console.log('Database connection disconnected'); 
+connection.on("connected", function () {
+  console.log("Database Connected ");
+});
+
+// If the connection throws an error
+connection.on("error", function (err) {
+  console.log("Database is not connected because of " + err);
+});
+
+// When the connection is disconnected
+connection.on("disconnected", function () {
+  console.log("Database connection disconnected");
+});
+// If the Node process ends, close the Mongoose connection
+process.on("close", function () {
+  connection.close(function () {
+    console.log(
+      "Mongoose default connection disconnected through app termination"
+    );
+    process.exit(0);
   });
-  // If the Node process ends, close the Mongoose connection 
-     process.on('close', function() {  
-     connection.close(function () { 
-      console.log('Mongoose default connection disconnected through app termination'); 
-      process.exit(0); 
-    }); 
-  }); 
+});
 //session config
-app.use(session({
-  secret:process.env.COOKIE_SECRET,
-  resave:false,
-  store:MongoDbStore.create({
-    mongoUrl: dburl
-}),
-  saveUninitialized:false,
-  cookie:{maxAge:1000*60*60*24}
-}));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: MongoDbStore.create({
+      mongoUrl: dburl,
+    }),
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+  })
+);
 //session storing
 /*let mongoStore = new MongoDbStore({
   mongooseConnection: connection,
   collection: 'sessions'
 })*/
 
-app.use(flash())
+app.use(flash());
 //Assets
-app.use(express.static('public'))
-app.use(express.json())
-require('./routes/web')(app)
-
+app.use(express.static("public"));
+app.use(express.json());
+//
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+require("./routes/web")(app);
 
 //Template Engine Setup
-app.use(expressLayout)
-app.set('views',path.join(__dirname,'/resources/views'))
-app.set('views engine','ejs')
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("views engine", "ejs");
 
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-})
+  console.log(`listening on port ${PORT}`);
+});
